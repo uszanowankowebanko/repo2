85b6c55db4438e7b07d04131877b7dac6c4b7f94
lekcjatest <pccyra@student.agh.edu.pl>
2025-05-29 23:07:28 +0200

    calosc

--- src/main.js
+++ src/main.js
@@ -1,24 +1,67 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+window.onload = async () => {
+  await api()
+}
+const api = async () => {
+  try {
+    const response = await fetch('https://wojrxoqafknjrbtxaqfx.supabase.co/rest/v1/article?select=*', {
+      headers: {
+        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvanJ4b3FhZmtuanJidHhhcWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NjYxMzgsImV4cCI6MjA2MzI0MjEzOH0.i7VW4Gi2fbesgXRflGr4cV6O6Z-N6pUbP6GjSY7OSLQ',
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Blad")
+    }
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="text-2xl font-bold text-primary"  >Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button" class="bg-blue-300 p-4" ></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    let dane = await response.json()
+    console.log(dane)
+    for (let i = 0; i < dane.length; i++) {
+      const p = document.createElement("p")
+      p.innerHTML = `
+          <p>tytul:${dane[i].title}</p></br>
+          <p>autor:${dane[i].author}</p></br>
+          <p>podtytul:${dane[i].subtitle}</p></br>
+          <p>data utworzenia artykulu:${dane[i].created_at}</p></br>
+          <p>zawartosc${dane[i].content}:</p>`
+          document.getElementById("app").appendChild(p)
+      p.style = "border:1px solid red"
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
 
-setupCounter(document.querySelector('#counter'))
+}
+
+const dodawanie = async () => {
+  let title = document.getElementById("author").value
+  let subtitle = document.getElementById("subtitle").value
+  let author = document.getElementById("author").value
+  let content = document.getElementById("content").value
+  let obiekt = {
+    title: title,
+    subtitle: subtitle,
+    author: author,
+    content: content,
+    
+  }
+  console.log(obiekt)
+  try {
+    const response = await fetch('https://wojrxoqafknjrbtxaqfx.supabase.co/rest/v1/article', {
+      method:"POST",
+      headers: {
+        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvanJ4b3FhZmtuanJidHhhcWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NjYxMzgsImV4cCI6MjA2MzI0MjEzOH0.i7VW4Gi2fbesgXRflGr4cV6O6Z-N6pUbP6GjSY7OSLQ'
+        , "Content-Type":"application/json"
+      },
+      body: JSON.stringify(obiekt)
+    });
+    if (!response.ok) {
+      throw new Error("Blad")
+    }
+    console.log(response.json())
+
+  } catch (error) {
+    console.error(error.message)
+  }
+
+}
+
+document.getElementById("dodaj").addEventListener("click", async (e) => await dodawanie()) 
